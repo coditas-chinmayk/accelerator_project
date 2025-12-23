f6d576a53df34551940630cf96a59009983c6938
coditas-chinmayk <chinmay.kale@coditas>
2025-12-23 11:45:16 +0530

    initial commit

---
+++ src/test/java/com/example/accelerator/AcceleratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accelerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcceleratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
